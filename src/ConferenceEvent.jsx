03fb985e3c188b275c8ba1b1c85dee77620d8aa0
zocalop <jcb2861@gmail.com>
2026-05-31 21:27:41 -0300

    Updated ConferenceEvent.jsx in main

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -140,25 +140,33 @@ const ConferenceEvent = () => {
                     </div>
                   </div>
           ))}
-              </div>
-          <div className="total_cost">Total Cost: ${venueTotalCost}</div>
+        </div>
+        <div className="total_cost">Total Cost: ${venueTotalCost}</div>
         </div>
 
-                            {/*Necessary Add-ons*/}
-                            <div id="addons" className="venue_container container_main">
-
-
-                                <div className="text">
-
-                                    <h1> Add-ons Selection</h1>
-
-                                </div>
-                                <div className="addons_selection">
-
-                                </div>
-                                <div className="total_cost">Total Cost:</div>
-
-                            </div>
+        {/*Necessary Add-ons*/}
+        <div id="addons" className="venue_container container_main">
+          <div className="text">
+            <h1> Add-ons Selection</h1>
+          </div>
+          <div className="addons_selection">
+            {avItems.map((item, index) => (
+              <div className="av_data_venue_main" key={index}>
+                <div className="img">
+                  <img src={item.img} alt={item.name} />
+                </div>
+                <div className="text"> {item.name}</div>
+                <div> ${item.cost} </div>
+                <div className="addons_btn">
+                  <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                  <span className="quantity-value">{item.quantity}</span>
+                  <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="total_cost">Total Cost:</div>
+          </div>
 
                             {/* Meal Section */}
 
