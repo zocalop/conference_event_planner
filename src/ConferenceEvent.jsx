d25b6be16d96eaf61d107d3908eb1aa83057019a
zocalop <jcb2861@gmail.com>
2026-06-02 13:17:32 -0300

    Updated CenferenceEvent.jsx in main

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -4,11 +4,14 @@ import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
 import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
+import { toggleMealSelection } from "./mealsSlice";
+
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
     const avItems = useSelector((state) => state.av);
+    const mealsItems = useSelector((state) => state.meals);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -175,42 +178,29 @@ const ConferenceEvent = () => {
             ))}
           </div>
           <div className="total_cost">Total Cost: {avTotalCost}</div>
+        </div>
+
+{/* Meal Section */}
+        <div id="meals" className="venue_container container_main">
+          <div className="text">
+            <h1>Meals Selection</h1>
           </div>
-
-                            {/* Meal Section */}
-
-                            <div id="meals" className="venue_container container_main">
-
-                                <div className="text">
-
-                                    <h1>Meals Selection</h1>
-                                </div>
-
-                                <div className="input-container venue_selection">
-
-                                </div>
-                                <div className="meal_selection">
-
-                                </div>
-                                <div className="total_cost">Total Cost: </div>
-
-
-                            </div>
-                        </div>
-                    ) : (
-                        <div className="total_amount_detail">
-                            <TotalCost totalCosts={totalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
-                        </div>
-                    )
-                }
-
-
-
-
-            </div>
-        </>
-
-    );
+          <div className="input-container venue_selection">
+          </div>
+          <div className="meal_selection">
+          </div>
+          <div className="total_cost">Total Cost: </div>
+        </div>
+      </div>
+        ) : (
+          <div className="total_amount_detail">
+            <TotalCost totalCosts={totalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
+          </div>
+        )
+      }
+      </div>
+    </>
+  );
 };
 
 export default ConferenceEvent;
