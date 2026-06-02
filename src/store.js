db1d3f2ca20420b6b53962dbf0c5ac92e783cbfa
zocalop <jcb2861@gmail.com>
2026-06-02 13:02:09 -0300

    Updated store.js and mealsSlice.js in main

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice.js';
+import mealsReducer from './mealsSlice.js';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
     av: avReducer,
+    meals: mealsReducer,
   },
 });
