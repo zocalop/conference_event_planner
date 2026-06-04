10b8faef042ffa40388a5cdd0aeac5f3223de322
zocalop <jcb2861@gmail.com>
2026-06-04 14:38:04 -0300

    Updated ConferenceEvent.jsx in main

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -66,11 +66,48 @@ const ConferenceEvent = () => {
       });
       mealsItems.forEach((item) => {
         if (item.selected) {
+          const itemForDisplay = { ...item, type: "meals" };
+          if (item.numberOfPeople) {
+            itemForDisplay.numberOfPeople = numberOfPeople;
+          }
+          items.push(itemForDisplay);
+        }
+      });
+      return items;
     };
 
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
+      console.log(items);
+      return <>
+        <div className="display_box1">
+          {items.length === 0 && <p>No items selected</p>}
+          <table className="table_item_data">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Unit Cost</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.cost}</td>
+                  <td>
+                    {item.type === "meals" || item.numberOfPeople
+                    ? ` For ${numberOfPeople} people`
+                    : item.quantity}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      <>
     };
 
     const calculateTotalCost = (section) => {
